bc452854014d20c71590d8d8921074a22ad1775f
giannola <54441894+gioanila@users.noreply.github.com>
2024-12-10 09:43:05 -0300

    changes in tile map and character

--- main.ts
+++ main.ts
@@ -1,21 +1,21 @@
 tiles.setCurrentTilemap(tilemap`legel1`)
-let mySprite = sprites.create(img`
+let mySprite3 = sprites.create(img`
     . . . . . . 5 . 5 . . . . . . . 
     . . . . . f 5 5 5 f f . . . . . 
-    . . . . f 1 5 2 5 1 6 f . . . . 
-    . . . f 1 6 6 6 6 6 1 6 f . . . 
-    . . . f 6 6 f f f f 6 1 f . . . 
-    . . . f 6 f f d d f f 6 f . . . 
-    . . f 6 f d f d d f d f 6 f . . 
-    . . f 6 f d 3 d d 3 d f 6 f . . 
-    . . f 6 6 f d d d d f 6 6 f . . 
-    . f 6 6 f 3 f f f f 3 f 6 6 f . 
-    . . f f d 3 5 3 3 5 3 d f f . . 
-    . . f d d f 3 5 5 3 f d d f . . 
-    . . . f f 3 3 3 3 3 3 f f . . . 
-    . . . f 3 3 5 3 3 5 3 3 f . . . 
+    . . . . f 1 5 2 5 1 c f . . . . 
+    . . . f 1 c c c c c 1 c f . . . 
+    . . . f c c f f f f c 1 f . . . 
+    . . . f c f f d d f f c f . . . 
+    . . f c f d f d d f d f c f . . 
+    . . f c f d 3 d d 3 d f c f . . 
+    . . f c c f d d d d f c c f . . 
+    . f c c f 6 f f f f 6 f c c f . 
+    . . f f d 6 3 6 6 3 6 d f f . . 
+    . . f d d f 6 3 3 6 f d d f . . 
+    . . . f f 6 6 6 6 6 6 f f . . . 
+    . . . f 6 6 3 6 6 3 6 6 f . . . 
     . . . f f f f f f f f f f . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
-scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite3)
+scene.cameraFollowSprite(mySprite3)
